be93b4b630be12e02c654f14c8603fcad07dc9ce
Atul-Xalxo <atulxalxo619@gmail.com>
2025-04-09 02:09:38 +0530

    updated pin-controller

--- backend/controller/pin.controller.js
+++ backend/controller/pin.controller.js
@@ -1,3 +1,34 @@
-export const test = (req,res)=>{
-    return res.json("pin controller")
-}
+import Pin from "../models/pin.model.js";
+
+export const getPins = async (req, res) => {
+  try {
+    const pageNumber = Number(req.query.cursor) || 0;
+    const search = req.query.search || "";
+    const LIMIT = 21;
+    const pins = await Pin.find(
+      search
+        ? {
+            $or: [
+              { title: { $regex: search, $options: "i" } },
+              { tags: { $in: [search] } },
+            ],
+          }
+        : {}
+    )
+      .limit(LIMIT)
+      .skip(pageNumber * LIMIT);
+
+    const hasNextPage = pins.length === LIMIT;
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    res
+      .status(200)
+      .json({ pins, nextCursor: hasNextPage ? pageNumber + 1 : null });
+  } catch (error) {
+    console.log("Error fetching pins", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+
